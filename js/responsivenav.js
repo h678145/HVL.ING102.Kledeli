a863adf087534063df3d0512f52e1684ce0168f7
h678145 <678145@stud.hvl.no>
2023-10-19 10:47:23 +0200

    Even more updateder files

--- js/responsivenav.js
+++ js/responsivenav.js
@@ -1,21 +1,25 @@
 function responsiveNav() {
-    var x = document.getElementById("mynavbar");
-    if (x.className === "navbar") {
+  "use strict";
+  /*jslint browser:true */
+  var x = document.getElementById("mynavbar");
+  if (x.className === "navbar") {
       x.className += " responsive";
-    } else {
+  } else {
       x.className = "navbar";
-    }
   }
+}
 
 function funcDropdown() {
+  "use strict";
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
-window.onclick = function(e) {
+window.onclick = function (e) {
+  "use strict";
   if (!e.target.matches('.dropbtn')) {
-  var myDropdown = document.getElementById("myDropdown");
-    if (myDropdown.classList.contains('show')) {
-      myDropdown.classList.remove('show');
-    }
+      var myDropdown = document.getElementById("myDropdown");
+      if (myDropdown.classList.contains('show')) {
+          myDropdown.classList.remove('show');
+      }
   }
-}
+};
